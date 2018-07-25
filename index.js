dfa0d5edc60e34bc9e8f30dd64c171e94958a00b
Adam Moon <amoon@defisolutions.com>
2018-07-25 16:44:30 -0500

    upgrade

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 'use strict';
 
 const validateBoolOption = (name, value, defaultValue) => {
@@ -18,18 +12,11 @@ const validateBoolOption = (name, value, defaultValue) => {
   return value;
 };
 
-module.exports = function(api, opts) {
+module.exports = function(api, opts, env) {
   if (!opts) {
     opts = {};
   }
 
-  // This is similar to how `env` works in Babel:
-  // https://babeljs.io/docs/usage/babelrc/#env-option
-  // We are not using `env` because it’s ignored in versions > babel-core@6.10.4:
-  // https://github.com/babel/babel/issues/4539
-  // https://github.com/facebook/create-react-app/issues/720
-  // It’s also nice that we can enforce `NODE_ENV` being specified.
-  var env = process.env.BABEL_ENV || process.env.NODE_ENV;
   var isEnvDevelopment = env === 'development';
   var isEnvProduction = env === 'production';
   var isEnvTest = env === 'test';
@@ -92,7 +79,14 @@ module.exports = function(api, opts) {
       // don't work without it: https://github.com/babel/babel/issues/7215
       require('@babel/plugin-transform-destructuring').default,
       // class { handleClick = () => { } }
-      require('@babel/plugin-proposal-class-properties').default,
+      // Enable loose mode to use assignment instead of defineProperty
+      // See discussion in https://github.com/facebook/create-react-app/issues/4263
+      [
+        require('@babel/plugin-proposal-class-properties').default,
+        {
+          loose: true,
+        },
+      ],
       // The following two plugins use Object.assign directly, instead of Babel's
       // extends helper. Note that this assumes `Object.assign` is available.
       // { ...todo, completed: true }
@@ -118,6 +112,7 @@ module.exports = function(api, opts) {
           removeImport: true,
         },
       ],
+      
       // Adds syntax support for import()
       require('@babel/plugin-syntax-dynamic-import').default,
       isEnvTest &&
